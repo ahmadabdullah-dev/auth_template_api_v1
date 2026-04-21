383391d50dc212a8c1133b8055ba630185e13a99
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-04-21 23:13:47 +0300

    add MailKit mail settings configuration

---
+++ Infrastructure/Mail/Settings/MailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Mail.Settings;
+
+public sealed class MailSettings
+{
+    public string Host { get; init; } = string.Empty;
+    public int Port { get; init; }
+    public string Username { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+    public string FromName { get; init; } = string.Empty;
+    public string FromEmail { get; init; } = string.Empty;
+    public bool UseSsl { get; init; } = true;
+}
